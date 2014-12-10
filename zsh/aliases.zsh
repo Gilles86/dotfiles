5e074bacf02c5d61688dd589121ba82e934dac96
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2014-12-10 13:38:33 +0100

    Inlcuded fasd

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,3 +10,13 @@ alias l='ls'
 # # Aliases to protect against overwriting
 alias cp='cp -i'
 alias mv='mv -i'
+
+# fasd aliases
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,1 +1,3 @@
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin"
+export FASD_DIR=$HOME/dotfiles/bin/fasd
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/texbin":${FASD_DIR}
+
