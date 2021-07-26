620518d04d8fc171d979ee1841585e6555e36c11
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2021-07-26 08:39:32 +0200

    Move powerbar

--- zsh/powerlevel10k.zsh
+++ zsh/powerlevel10k.zsh
@@ -5,3 +5,4 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
