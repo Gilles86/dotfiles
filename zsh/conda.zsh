92668a0b55588179c230dd45f453066ed1da1849
Gilles de Hollander <gilles.de.hollander@gmail.com>
2025-12-05 15:36:28 +0100

    Update cona init

--- zsh/conda.zsh
+++ zsh/conda.zsh
@@ -1,14 +1,13 @@
 # >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
+# Minimal conda setup - avoid conda hook which has hardcoded old paths
 
 # Auto-detect conda installation location
-# Check common locations and environment variables
 if [ -n "$CONDA_PREFIX" ]; then
   CONDA_DIR="$CONDA_PREFIX"
-elif [ -d "$HOME/mambaforge" ]; then
-  CONDA_DIR="$HOME/mambaforge"
 elif [ -d "$HOME/miniforge3" ]; then
   CONDA_DIR="$HOME/miniforge3"
+elif [ -d "$HOME/mambaforge" ]; then
+  CONDA_DIR="$HOME/mambaforge"
 elif [ -d "$HOME/miniconda3" ]; then
   CONDA_DIR="$HOME/miniconda3"
 elif [ -d "$HOME/anaconda3" ]; then
@@ -17,17 +16,12 @@ elif [ -d "/opt/conda" ]; then
   CONDA_DIR="/opt/conda"
 fi
 
-# Only initialize if conda directory is found
-if [ -n "$CONDA_DIR" ] && [ -d "$CONDA_DIR/bin" ]; then
+# Only initialize if conda binary exists
+if [ -f "$CONDA_DIR/bin/conda" ]; then
   export PATH="$CONDA_DIR/bin:$PATH"
-  
-  # Run conda setup
-  __conda_setup="$($CONDA_DIR/bin/conda 'shell.zsh' 'hook' 2> /dev/null)"
-  if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-  elif [ -f "$CONDA_DIR/etc/profile.d/conda.sh" ]; then
+  # Source conda profile directly instead of using 'conda shell hook' which has stale paths
+  if [ -f "$CONDA_DIR/etc/profile.d/conda.sh" ]; then
     . "$CONDA_DIR/etc/profile.d/conda.sh"
   fi
-  unset __conda_setup
 fi
 # <<< conda initialize <<<
